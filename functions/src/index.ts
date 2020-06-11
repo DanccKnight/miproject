cd8c37044a6a69d57eae1a2ae8e1c399dcd52822
Ansuman Acharya <nitinansu12@gmail.com>
2020-06-11 16:17:00 +0530

    function to deploy a notification to caretakers

---
+++ functions/src/index.ts
@@ -0,0 +1,37 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+const db = admin.firestore();
+const fcm = admin.messaging();
+
+    export const sendToTheAdmins = functions.firestore
+    .document('Notifications/{notificationId}')
+    .onWrite(async snapshot => {
+        const patient = snapshot.after.data();
+        var tokens = [];
+
+        await db.collection('Admins').get()
+            .then((snapshot) => {
+
+                if(snapshot.empty)
+                    console.log('No devices');
+                else {
+
+                    for(var doc of snapshot.docs){
+                        tokens.push(doc.data().token)
+                    }
+                }
+            })
+
+        const payload = {
+            notification: {
+                title: `${patient.name} is calling for help`,
+                body: `Rush to Room No. ${patient.room_no} now!`,
+                clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+            },
+        };
+
+        return fcm.sendToDevice(tokens, payload);
+    });
+

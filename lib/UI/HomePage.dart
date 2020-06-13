9a2f531773b4ffb6af6f00b67158134f0cd30951
Ansuman Acharya <nitinansu12@gmail.com>
2020-06-13 20:33:10 +0530

    displayAbout() implemented

--- lib/UI/HomePage.dart
+++ lib/UI/HomePage.dart
@@ -63,7 +63,7 @@ class _HomePageState extends State<HomePage> {
                           top: 80,
                           left: MediaQuery.of(context).size.width - 230,
                           child: Text(
-                              "Welcome to the most low \npaying job of your life!",
+                              "Welcome to our family \nof caretakers!",
                               style: TextStyle(
                                 fontSize: 22,
                                 color: Colors.white,
@@ -280,8 +280,7 @@ class _HomePageState extends State<HomePage> {
                         Positioned(
                             top: 80,
                             left: MediaQuery.of(context).size.width - 230,
-                            child: Text(
-                                "Get to know more\n about us",
+                            child: Text("Get to know more \nabout us!",
                                 style: TextStyle(
                                   fontSize: 22,
                                   color: Colors.white,
@@ -291,6 +290,76 @@ class _HomePageState extends State<HomePage> {
                       ]),
                     )
                   ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Container(
+                height: 40,
+                width: 130,
+                decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.12),
+                    borderRadius: BorderRadius.circular(8)),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text("Our Mission",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      )),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+              child: Card(
+                elevation: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    "Many believe that it is our moral obligation to look after our elders, and we share your views. We feel that it is our duty to care for our elders responsibly, to treat them with dignity and to provide them with the highest level of care possible.",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(fontSize: 16),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25,top: 10),
+              child: Container(
+                height: 40,
+                width: 130,
+                decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.12),
+                    borderRadius: BorderRadius.circular(8)),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text("Our Services",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      )),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+              child: Card(
+                elevation: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    "We make use of cutting edge technology to provide holistic care that focuses on all aspects of an elderly's life. We believe in respecting others privacy, so we built our services in accordance with these views. Our systems run 24x7, without intruding on the privacy of the citizens.",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(fontSize: 16),
+                  ),
                 ),
               ),
             )

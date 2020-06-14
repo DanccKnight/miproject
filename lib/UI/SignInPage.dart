20acd5ce74729f905941729d249924d2d5ea49e1
Ansuman Acharya <nitinansu12@gmail.com>
2020-06-14 12:24:10 +0530

    updated UI

--- lib/UI/SignInPage.dart
+++ lib/UI/SignInPage.dart
@@ -12,22 +12,39 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 40,
-            child: SignInButton(Buttons.GoogleDark, onPressed: () async {
-              await Auth.signInWithGoogle().then((value) {
-                value == true
-                    ? Navigator.of(context).pushReplacementNamed('/HomePage')
-                    : Scaffold.of(context)
-                        .showSnackBar(SnackBar(content: Text("Signin failed!")));
-              });
-            }),
-          )
-        ],
-      )),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: FlutterLogo(size: 150),
+              ),
+              Container(
+                  padding: EdgeInsets.fromLTRB(70.0, 100.0, 70.0, 0.0),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.blue,
+                    child: MaterialButton(
+                      minWidth: MediaQuery.of(context).size.width,
+                      onPressed: () async {
+                        await Auth.signInWithGoogle().then((value) {
+                          value == true
+                              ? Navigator.of(context)
+                              .pushReplacementNamed('/HomePage')
+                              : Scaffold.of(context).showSnackBar(
+                              SnackBar(content: Text("Signin failed")));
+                        });
+                      },
+                      child: Text("Login with Google",
+                          textAlign: TextAlign.center,
+                          style: TextStyle().copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  )),
+            ]),
+      ),
     );
   }
 }
